e1771abb1de80fb155dcae10edafb514917264a5
Durval Moreira <duurval@gmail.com>
2014-07-23 10:08:29 -0300

    Criação da base do tema

---
+++ header.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till #main div
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="no-js ie ie7 lt-ie9 lt-ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="no-js ie ie8 lt-ie9" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8) ]><!-->
+<html class="no-js" <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png" rel="shortcut icon" />
+	<link href='http://fonts.googleapis.com/css?family=Maven+Pro:400,500,700,900' rel='stylesheet' type='text/css'>
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<div class="container">
+		<header id="header" role="banner">
+			
+				<div class="site-title h1"><a href="<?php echo home_url(); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
+				<div class="site-description h2"><?php bloginfo( 'description' ); ?></div>
+			
+
+			<?php
+				$header_image = get_header_image();
+				if ( ! empty( $header_image ) ) :
+			?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" height="<?php esc_attr_e( $header_image->height ); ?>" width="<?php esc_attr_e( $header_image->width ); ?>" alt="" /></a>
+			<?php endif; ?>
+
+			<nav id="main-navigation" class="navbar navbar-default" role="navigation">
+				<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'odin' ); ?>"><?php _e( 'Skip to content', 'odin' ); ?></a>
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-navigation">
+					<span class="sr-only"><?php _e( 'Toggle navigation', 'odin' ); ?></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<?php /*
+
+					<a class="navbar-brand" href="<?php echo home_url(); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+
+					*/ ?>
+				</div>
+
+				<div class="collapse navbar-collapse navbar-main-navigation">
+					<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'main-menu',
+								'depth'          => 2,
+								'container'      => false,
+								'menu_class'     => 'nav navbar-nav',
+								'fallback_cb'    => 'Odin_Bootstrap_Nav_Walker::fallback',
+								'walker'         => new Odin_Bootstrap_Nav_Walker()
+							)
+						);
+					?>
+
+					<form method="get" class="navbar-form navbar-right" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+						<label for="navbar-search" class="sr-only"><?php _e( 'Search:', 'odin' ); ?></label>
+						<div class="form-group">
+							<input type="text" class="form-control" name="s" id="navbar-search" />
+						</div>
+						<button type="submit" class="btn btn-default"><?php _e( 'Search', 'odin' ); ?></button>
+					</form>
+				</div><!-- .navbar-collapse -->
+			</nav><!-- #main-menu -->
+		</header><!-- #header -->
+
+		<div id="main" class="site-main row">
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme and one of the
+ * two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="<?php echo odin_page_sidebar_classes(); ?>">
+		<div id="content" class="site-content" role="main">
+
+			<?php
+				if ( have_posts() ) :
+					// Start the Loop.
+					while ( have_posts() ) : the_post();
+
+						/*
+						 * Include the post format-specific template for the content. If you want to
+						 * use this in a child theme, then include a file called called content-___.php
+						 * (where ___ is the post format) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+
+					endwhile;
+
+					// Post navigation.
+					odin_paging_nav();
+
+				else :
+					// If no content, include the "No posts found" template.
+					get_template_part( 'content', 'none' );
+
+				endif;
+			?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
