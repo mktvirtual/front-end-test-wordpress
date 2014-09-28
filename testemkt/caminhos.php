f4b4ffcbb076ac62d7a8ec81deb9062ce7bff4f9
William Costa <williamcosta.design@gmail.com>
2014-09-28 02:22:30 -0300

    Tema para o teste de Front-end

--- testemkt/caminhos.php
+++ testemkt/caminhos.php
@@ -4,5 +4,4 @@ $srequest= $_SERVER['REQUEST_URI'];
 $caminho="http://localhost/teste-mkt/wp-content/themes/testemkt/";
 $caminhocss="http://localhost/teste-mkt/wp-content/themes/testemkt/"; 
 $url="http://localhost/teste-mkt/";
-
 ?>
