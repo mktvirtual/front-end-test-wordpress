0e5a731959a19c2f2af5499fd78baaab2c099d99
brugo <brugohb@gmail.com>
2014-10-19 19:28:32 -0200

    updates

---
+++ footer.php
@@ -0,0 +1,15 @@
+<div id="footer">
+
+	<div id="icons" class="fleft">
+		<img src="<?php bloginfo('template_url'); ?>/images/sico1.jpg"></img>
+		<img src="<?php bloginfo('template_url'); ?>/images/sico2.jpg"></img>
+		<img src="<?php bloginfo('template_url'); ?>/images/sico3.jpg"></img>
+		<img src="<?php bloginfo('template_url'); ?>/images/sico4.jpg"></img>
+		<img src="<?php bloginfo('template_url'); ?>/images/sico5.jpg"></img>
+	</div>
+	
+	<div id="logofooter" class="fright">
+		<img src="<?php bloginfo('template_url'); ?>/images/logofooter.jpg"></img>
+	</div>
+			
+</div>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?> >
+	<head>
+		<meta charset="<?php bloginfo('charset') ?>" />
+		<title><?php wp_title('-',true,'right'); bloginfo() ?></title>
+		
+		<link rel="stylesheet" type="text/css" href=" <?php bloginfo('template_url') ?>/style.css" />				
+	
+		<?php wp_head(); ?>
+	</head>
+	<body>	
+		<div id="container">
+			<div id="header">
+			</div>
