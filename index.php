97da35f98e357c1cae864f8487dae9ba46969152
jorgehard <jorgehenrique@live.com>
2016-02-25 13:26:36 -0300

    Fix Plugins (Jquery, Bootstrap, Font-Awesome)
    CDN & DOWNLOAD

--- index.php
+++ index.php
@@ -5,9 +5,15 @@
 		<meta name="viewport" content="width=device-width" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<title>Home - MKT Virtual</title>
+		
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+		
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+		
 		<link rel="stylesheet" href="css/style.css"/>
+		
+		<!-- <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"/> If you are OFFLINE (CDN)--> 
+		<!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/> If you are OFFLINE (CDN)--> 
 	</head>
 	<body>
 		<header>
@@ -222,9 +228,9 @@
 				</div>
 			</div>
 		</footer>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/jquery-1.12.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 		<script src="js/jquery.maskedinput.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 		<script src="js/main.js"></script>
 	</body>
 </html>
