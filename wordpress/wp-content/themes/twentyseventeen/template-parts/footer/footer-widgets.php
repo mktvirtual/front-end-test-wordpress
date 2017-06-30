f37a90392164d6c601e22d8745e04b33ea499e0e
feeloi <fe_nando92@hotmail.com>
2017-06-29 23:48:52 -0300

    Teste Font-end Mkt Virtual.

---
+++ wordpress/wp-content/themes/twentyseventeen/template-parts/footer/footer-widgets.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Displays footer widgets if assigned
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="row">
+ <div class="col-md-12 col-xs-4 footer">
+        <div class="col-md-4 lojas">
+          <ul><a href="#">Lojas Logo
+            <li><a href="#">Sobre</li>
+            <li><a href="#">Lojas</li>
+            <li><a href="#">Trabalhe Conosco</li>
+            <li><a href="#">Contato</li>
+          </ul>
+        </div>
+            
+        <div class="col-md-4 lista">
+          <ul><a href="#">Listas de Atalhos
+            <li><a href="#">Portal do Colaborador</li>
+            <li><a href="#">Promoções</li>
+            <li><a href="#">Cartão do Cliente</li>
+            <li><a href="#">Cadastre-se</li>
+            <li><a href="#">Blog</li>
+          </ul>
+        </div>
+
+        <div class="col-md-2 sac">
+          <a href="#">SAC LOGO
+        </div>
+
+         <div class="col-md-2 tel">
+          <a href="#">0800 170 2100
+        </div>     
+  <!-- FOOTER -->
+      </div>
+
+
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</div>
+
+ 
---
+++ wordpress/wp-content/themes/twentyseventeen/template-parts/header/header-image.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Displays header media
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<title>Teste MKT</title>
+  <link rel="stylesheet" type="text/css" href="<?php echo bloginfo('url') ?>\wp-content\themes\twentyseventeen\assets\css\bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo bloginfo('url') ?>\wp-content\themes\twentyseventeen\assets\css\style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  	 <div class="header col-md-12 col-xs-6">  
+          <ul> 
+            <div class="col-md-2 col-xs-2 col-sm-2">       
+              <li><a href="#">Peça seu cartão cliente</a></li>
+            </div>  
+
+            <div class="col-md-2 col-xs-2 col-sm-2">    
+              <li><a href="#">Solicite a 2 via do boleto</a></li>     
+            </div>
+
+            <div class="col-md-2 col-xs-2 col-sm-2"> 
+              <li><a href="#">Encontre uma loja</a></li>        
+            </div>
+
+            <div class="col-md-2 col-xs-2 col-sm-2"> 
+              <li><a href="#">Assine a NewsLetter</a></li>   
+            </div>
+
+            <div class="col-md-2 col-xs-2 col-sm-2"> 
+              <li><a href="#">Assine a NewsLetter</a></li> 
+            </div>
+
+            <div class="col-md-2 col-xs-6 col-sm-6" style="float: right;"> 
+              <input class="pesquisa" type="search" ">   
+            </div>
+
+          </ul>   
+          <div class="col-xs-6 col-sm-6">
+          <select class="drop">
+          	<option><a href="#">Peça seu cartão cliente</a></option>
+          	<option><a href="#">Solicite a 2 via do boleto</a></option>
+          	<option><a href="#">Encontre uma loja</a></option>
+          	<option><a href="#">Assine a NewsLetter</a></option>
+          	<option><a href="#">Assine a NewsLetter</a></option>
+          </select>
+          </div>
+</div>   
+
+
+
